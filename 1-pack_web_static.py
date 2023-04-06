3263bbafba396f562852ed527d9b553c044d13b7
Olamide <olire96@gmail.com>
2023-04-06 23:27:18 +0100

    second task

---
+++ 1-pack_web_static.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""A fab file to archive a folder"""
+from datetime import datetime
+from fabric.api import local
+from fabric.decorators import runs_once
+
+
+# Prototype: def do_pack():
+@runs_once
+def do_pack():
+    timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+    archive_name = "web_static_" + timestamp + ".tgz"
+    local("mkdir -p versions")
+    result = local("tar czf versions/{} web_static".format(archive_name))
+    if result.failed:
+        return None
+    return "versions/{}".format(archive_name)
