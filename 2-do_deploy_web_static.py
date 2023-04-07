7240b50194879fd1822223971e8edc3a60022431
Olamide <olire96@gmail.com>
2023-04-07 18:15:21 +0100

    added do_pack

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,6 +9,18 @@ import os
 env.hosts = ['34.202.159.232', '100.24.236.27']
 
 
+@runs_once
+def do_pack():
+    """The funcion to do the archiving"""
+    timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+    archive_name = "web_static_" + timestamp + ".tgz"
+    local("mkdir -p versions")
+    result = local("tar czf versions/{} web_static".format(archive_name))
+    if result.failed:
+        return None
+    return "versions/{}".format(archive_name)
+
+
 def do_deploy(archive_path):
     """Distribute an archive to the remote hosts"""
     # Returns False if the file at the path archive_path doesn’t exist
