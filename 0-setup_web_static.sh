9c26a4fc810f8235e866695bea651f5653e596ff
kelvin onumajuru <kelvinonumajuru@gmail.com>
2023-04-09 03:31:27 -0700

    task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -22,15 +22,6 @@ sudo ln -sfn /data/web_static/releases/test/ /data/web_static/current
 # Make the user and group ubuntu the owner of the file
 sudo chown -R ubuntu:ubuntu /data/
 # Edit the default file to make /hbnb_static point to a directory
-# Create a temporary file containing the configuration block
-cat <<'EOF' > /tmp/temp_block
-        location /hbnb_static/ {
-            alias /data/web_static/current/;
-        }
-EOF
-# Insert the contents of the temporary file after the 10th line
-sudo sed -i '10r /tmp/temp_block' /etc/nginx/sites-available/default
-# Remove the temporary file
-rm /tmp/temp_block
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 # Restart Nginx
 sudo service nginx restart
