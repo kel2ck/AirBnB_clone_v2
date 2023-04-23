00b21c23d376ed8057b7115f07b30d4eaf94e252
kel2ck <kelvinonumajuru@gmail.com>
2023-04-23 16:08:05 -0700

    update state.py

--- models/state.py
+++ models/state.py
@@ -4,22 +4,26 @@ from models.base_model import BaseModel, Base
 from models import storage_type
 from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.orm import relationship
+import shlex
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    if storage_type == "db":
-        cities = relationship("City", backref="state",
-                              cascade="all, delete, delete-orphan")
-    else:
-        @property
-        def cities(self):
-            from models import storage
-            related_cities = []
-            cities = storage.all(City)
-            for city in cities.values():
-                if city.state_id == self.id:
-                    related_cities.append(city)
-            return related_cities
+    cities = relationship("City", cascade="all, delete, delete-orphan",
+                          backref="state")
+
+    @property def cities(self):
+        values_city = models.storage.all()
+        list_city = []
+        result = []
+        for key in values_city:
+            city = key.replace('.', ' ')
+            city = shlex.split(city)
+            if (city[0] == 'City'):
+                list_city.append(values_city[key])
+        for elem in list_city:
+            if (elem.state_id == self.id):
+                result.append(elem)
+        return (result)
