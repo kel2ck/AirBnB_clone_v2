86fda279af77fb14a15eb646a8e509bc853a0ae9
kel2ck <kelvinonumajuru@gmail.com>
2023-04-23 01:42:25 -0700

    task 4

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -4,11 +4,12 @@
 Routes:
     /: display “Hello HBNB!”
     /hbnb: display "HBNB".
-    /c/<text>: display “C ” followed by the value of the text variable.
-    /python/(<text>): display “Python ”, followed by the value of the text.
-    - The default value of text is “is cool”
+    /c/<text>: display “C ” followed by the value of the text variable
+    /python/(<text>): display “Python ”, followed by 
+        the value of the text variable
+        The default value of text is “is cool”
     /number/<n>: display “n is a number” only if n is an integer.
-- strict_slashes=False is mandatory in route definition.
+- strict_slashes=False is mandatory in route definition
 """
 from flask import Flask
 
@@ -30,17 +31,17 @@ def c(text):
     text = text.replace("_", " ")
     return "C {}".format(text)
 
+
 @app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python(text="is cool"):
     text = text.replace("_", " ")
     return "Python {}".format(text)
 
-@app.route('/python/', strict_slashes=False)
-@app.route('/python/<text_py>', strict_slashes=False)
-def python(text_py='is cool'):
-    text = text.replace("_", " ")
-    return('Python {}'.format(text)
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_route(n):
+    return('{} is a number'.format(n))
 
 
 if __name__ == '__main__':
