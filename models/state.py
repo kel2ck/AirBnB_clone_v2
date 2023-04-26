9ea0a489b63c8b83b65c5a3005bf9e2383611dab
kel2ck <kelvinonumajuru@gmail.com>
2023-04-26 09:15:30 -0700

    update file

--- models/state.py
+++ models/state.py
@@ -1,33 +1,29 @@
 #!/usr/bin/python3
-"""Defines the State class."""
-import models
-from os import getenv
-from models.base_model import Base
-from models.base_model import BaseModel
-from models.city import City
-from sqlalchemy import Column
-from sqlalchemy import String
+""" State Module for HBNB project """
+from models.base_model import BaseModel, Base
+from models import storage_type
+from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.orm import relationship
+import shlex
 
 
 class State(BaseModel, Base):
-    """Represents a state for a MySQL database.
-    Inherits from SQLAlchemy Base and links to the MySQL table states.
-    Attributes:
-        __tablename__ (str): The name of the MySQL table to store States.
-        name (sqlalchemy String): The name of the State.
-        cities (sqlalchemy relationship): The State-City relationship.
-    """
+    """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City",  backref="state", cascade="delete")
+    cities = relationship("City", cascade="all, delete, delete-orphan",
+                          backref="state")
 
-    if getenv("HBNB_TYPE_STORAGE") != "db":
-        @property
-        def cities(self):
-            """Get a list of all related City objects."""
-            city_list = []
-            for city in list(models.storage.all(City).values()):
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+    @property def cities(self):
+        values_city = models.storage.all()
+        list_city = []
+        result = []
+        for key in values_city:
+            city = key.replace('.', ' ')
+            city = shlex.split(city)
+            if (city[0] == 'City'):
+                list_city.append(values_city[key])
+        for elem in list_city:
+            if (elem.state_id == self.id):
+                result.append(elem)
+        return (result)
